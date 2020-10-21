46aca3a4982c0e9bca3e753af5b8fbaf3306e21a
edgar.klerks <edgar.klerks@sanoma.com>
2020-10-21 12:19:27 +0200

    change some stuff

--- src/Model.hs
+++ src/Model.hs
@@ -1,4 +1,4 @@
-{-#LANGUAGE TemplateHaskell #-}
+{-#LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, OverloadedStrings #-}
 module Model where
 
 import qualified Data.Text as T
@@ -7,26 +7,36 @@ import Data.FuzzySet.Types
 import qualified Data.Map as M
 import qualified Data.ByteString as B
 import Data.Aeson.TH
- 
 
+import Control.Monad.Trans
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Except
+import Control.Monad.Except
+ 
 newtype Secret = Secret { secret :: B.ByteString }  
 
-data Database = Database {
-      path :: String
-    , databaseSecret :: Secret
-    , tables :: M.Map String Table 
-} 
-
-data Table = Table { 
+data Record = Record { 
       fuzzy :: M.Map T.Text FuzzySet
-    , tableName :: String
+    , recordName :: String
     , keywords :: [T.Text]
     , tags :: M.Map T.Text Bool
     , text :: T.Text
-    , tweak :: T.Text
 }
 
-$(deriveJSON defaultOptions ''Table)
+data Env = Env {
+    currentRecordName :: String,
+    currentDatabasePath :: FilePath
+}
+
+emptyRecord :: Record
+emptyRecord = Record M.empty "" [] M.empty "" 
+
+newtype DBMonad a = DBMonad { runDBMonad :: ReaderT Env (ExceptT String IO) a}
+        deriving (Monad, Functor, Applicative, MonadIO, MonadError String, MonadReader Env)
+
+
+$(deriveJSON defaultOptions ''Record)
 $(deriveJSON defaultOptions ''FuzzySet)
 $(deriveJSON defaultOptions ''GramInfo)
 $(deriveJSON defaultOptions ''FuzzySetItem)

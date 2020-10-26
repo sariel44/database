bc751e23ade0ebf7cf9062f495373f5e75f1d43f
sariel44 <sariel44@protonmail.com>
2020-10-26 18:54:15 +0100

    added first rest methods

--- src/WebLib.hs
+++ src/WebLib.hs
@@ -1,10 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module WebLib where
 
+import qualified Shelly as S
+import qualified Data.Text as T 
+import Control.Monad.Trans
+
+
 import Web.Scotty
+import Model as M
+import Lib as L 
 
 
+main :: IO ()
 main = scotty 3000 $ do 
+
+    get "/db" $ do
+        xs <- liftIO $ S.shelly $ T.lines <$> S.run "ls" ["../templates/"] 
+        json xs
+
+    get "/db/:database/" $ do 
+        name <- param "database"
+        xs <- liftIO $ evalDBMonad (listDB) (Env name "")
+        json xs
+
+
     get "/show/:database/:name" $ do 
         database <- param "database"
         name <- param "word"

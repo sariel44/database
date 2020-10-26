9e3f002f48dcdf31c882c072f3560b4d9aea65cc
sariel44 <sariel44@protonmail.com>
2020-10-26 19:26:15 +0100

    added hamlet templates

--- src/WebLib.hs
+++ src/WebLib.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module WebLib where
 
 import qualified Shelly as S
 import qualified Data.Text as T 
 import Control.Monad.Trans
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
 
 
 import Web.Scotty
-import Model as M
+import qualified Model as M
 import Lib as L 
 
+import Text.Hamlet
 
 main :: IO ()
 main = scotty 3000 $ do 
@@ -23,18 +29,18 @@ main = scotty 3000 $ do
 
     get "/db/:database" $ do 
         name <- param "database"
-        xs <- liftIO $ evalDBMonad (listDB) (Env name "")
+        xs <- liftIO $ M.evalDBMonad (listDB) (M.Env name "")
         json xs
 
     get "/db/:database/:name" $ do
         database <- param "database"
         name <- param "name"
-        xs <- liftIO $ evalDBMonad (load name :: DBMonad Record) (Env database name)
+        xs <- liftIO $ M.evalDBMonad (load name :: M.DBMonad M.Record) (M.Env database name)
         json xs
 
 
 
-    get "/show/:database/:name" $ do 
-        database <- param "database"
-        name <- param "word"
-        html $ "<h1>Reading " <> database <> " and " <> name <> "</h1>"
+    get "/" $ do 
+        html $ renderHtml (
+            [shamlet| <h1>hello world</h1>
+        |])  

e66060ab0af0298fb61db1cf34853728aebf7a9a
sariel44 <sariel44@protonmail.com>
2020-10-28 15:45:37 +0100

    added crap-a

---
+++ src/WebTemplates.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+module WebTemplates where 
+import Text.Hamlet
+import qualified Data.Text as T 
+import Text.Blaze
+
+
+defaultRender :: r -> [(T.Text,T.Text)] -> T.Text
+defaultRender _ _ = ""
+
+indexHTML :: Render () -> Html
+indexHTML = $(hamletFile  "webtemplates/index.html")  
